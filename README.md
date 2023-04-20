4b349e04a024fbcfe08733cc0cfc0fc91f3be325
JohannesDienst-askui <johannes.dienst@askui.com>
2023-04-20 11:32:56 +0200

    Initial commit: Only works for mouseLeftClick()

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # eslint-plugin-askui
+
+Validate correct usage of askui library
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `@askui/eslint-plugin`:
+
+```sh
+npm install @askui/eslint-plugin --save-dev
+```
+
+## Usage
+
+Add `askui` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "@askui"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "@askui/no-missing-askui-exec": "error"
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+TODO: Run eslint-doc-generator to generate the rules list.
+<!-- end auto-generated rules list -->
+
+
---
+++ tests/lib/rules/no-missing-askui-exec.spec.js
@@ -0,0 +1,48 @@
+// no-missing-askui-exec.spec.js
+const { RuleTester } = require('eslint');
+const noMissingAskuiExecRule = require('../../../lib/rules/no-missing-askui-exec.js');
+const ruleTester = new RuleTester({
+    parser: require.resolve('@typescript-eslint/parser'), // eslint-disable-line
+  });
+ruleTester.run(
+    'no-missing-askui-exec',
+    noMissingAskuiExecRule,
+    {
+    valid: [
+        {
+            code: 'await aui.mouseLeftClick().button().exec()',
+        },
+        {
+            code: 'await aui.mouseLeftClick().exec()',
+        },
+        {
+            code: 'await aui.mouseLeftClick().button().withText("42").exec()',
+        },
+    ],
+    invalid: [
+        {
+            code: 'await aui.mouseLeftClick().button().exec',
+            errors: [{ messageId: 'missingAskuiExec' }],
+        },
+        {
+            code: 'await aui.mouseLeftClick().exec',
+            errors: [{ messageId: 'missingAskuiExec' }],
+        },
+        {
+            code: 'await aui.mouseLeftClick().button().withText("42").exec',
+            errors: [{ messageId: 'missingAskuiExec' }],
+        },
+        {
+            code: 'await aui.mouseLeftClick().exec',
+            errors: [{ messageId: 'missingAskuiExec' }],
+        },
+        {
+            code: 'await aui.mouseLeftClick',
+            errors: [{ messageId: 'missingAskuiExec' }],
+        },
+        {
+            code: 'await aui.mouseLeftClick()',
+            errors: [{ messageId: 'missingAskuiExec' }],
+        }
+    ]
+});
